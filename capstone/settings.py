13c1c2ffebb9449bd93c3dfb00f10e9e63a4bb74
Clement Choi <clementchoi1997@gmail.com>
2022-12-11 23:44:34 +0900

    change db

--- capstone/settings.py
+++ capstone/settings.py
@@ -17,7 +17,6 @@ load_dotenv()  # loads the configs from .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -76,10 +75,20 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'my_db',
+    'USER' : 'hero',
+    'PASSWORD' : 'my_db@123',
+    'HOST' : 'localhost',
+    'PORT' : '5432',
     }
 }
 
